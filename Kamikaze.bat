0b9279fed3d45435de667a640a2f942e38929b62
João Estêvão <jonhgstephen@gmail.com>
2023-05-23 13:36:38 +0100

    Create Kamikaze.bat

---
+++ Kamikaze.bat
@@ -0,0 +1,43 @@
+REM Made by ComPlexU
+@echo off
+cd /d "%~dp0"
+
+set output_dir=%~dp0\output
+
+REM Creates the output directory if it does not exist
+if not exist "%output_dir%" mkdir "%output_dir%"
+
+REM Gets the current date and time
+echo the current time is: %date% - %time% >> "%output_dir%\time.txt"
+REM User and system information 
+ver >> "%output_dir%\ver.txt"
+set >> "%output_dir%\set.txt"
+Net User >> "%output_dir%\netuser.txt"
+net sessions >> "%output_dir%\netsessions.txt"
+reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName >> "%output_dir%\productname.txt"
+REM Encryption and bitlocker
+manage-bde -status >> "%output_dir%\manage_bde.txt"
+manage-bde -protectors -get C: >> "%output_dir%\bitlocker.txt"
+REM Processes and Applications
+Tasklist /v >> "%output_dir%\tasklist.txt"
+Wmic process get name,processid,priority,threadcount,privatepagecount >> "%output_dir%\process.txt"
+REM Scheduled tasks and events
+schtasks >> "%output_dir%\schtasks.txt"
+wevtutil el >> "%output_dir%\wevtutil.txt"
+REM Get the last ctrl c Clipboard	
+powershell -command "Get-Clipboard" >> "%output_dir%\area_transferencia.txt"
+REM Network Information
+ipconfig /all >> "%output_dir%\ipconfig_all.txt"
+ipconfig /displaydns >> "%output_dir%\displaydns.txt"
+Netstat -a >> "%output_dir%\netstat_a.txt"
+Arp -a >> "%output_dir%\arp_a.txt"
+Netsh int ipv6 show neigh >> "%output_dir%\ipv6_neigh.txt"
+Netsh wlan show all >> "%output_dir%\wlan_all.txt"
+Route print >> "%output_dir%\route_print.txt"
+Netstat -c >> "%output_dir%\netstat_c.txt"
+Netstat -s >> "%output_dir%\netstat_s.txt"
+Rasdial >> "%output_dir%\rasdial.txt"
+Net share >> "%output_dir%\net_share.txt"
+Netsh wlan show profiles >> "%output_dir%\wlan_profiles.txt"
+Nslookup -d >> "%output_dir%\nslookup.txt"
+exit 0
