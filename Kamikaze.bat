a8b00667ac474ccc712744c88148bd323dbcf747
João Estêvão <jonhgstephen@gmail.com>
2023-05-26 21:58:39 +0100

    Update Kamikaze.bat

--- Kamikaze.bat
+++ Kamikaze.bat
@@ -1,15 +1,15 @@
-:Made by ComPlexU
+::Made by ComPlexU
 @echo off
 cd /d "%~dp0"
 
 set output_dir=%~dp0\output
 
-:Creates the output directory if it does not exist
+::Creates the output directory if it does not exist
 if not exist "%output_dir%" mkdir "%output_dir%"
 
-:Gets the current date and time
+::Gets the current date and time
 echo the current time is: %date% - %time% >> "%output_dir%\time.txt"
-:User and system information 
+::User and system information 
 ver >> "%output_dir%\ver.txt"
 set >> "%output_dir%\set.txt"
 Net User >> "%output_dir%\netuser.txt"
@@ -18,15 +18,15 @@ reg query "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion" /v ProductName >> 
 :Encryption and bitlocker
 manage-bde -status >> "%output_dir%\manage_bde.txt"
 manage-bde -protectors -get C: >> "%output_dir%\bitlocker.txt"
-:Processes and Applications
+::Processes and Applications
 Tasklist /v >> "%output_dir%\tasklist.txt"
 Wmic process get name,processid,priority,threadcount,privatepagecount >> "%output_dir%\process.txt"
-:Scheduled tasks and events
+::Scheduled tasks and events
 schtasks >> "%output_dir%\schtasks.txt"
 wevtutil el >> "%output_dir%\wevtutil.txt"
-:Get the last ctrl c Clipboard	
+::Get the last ctrl c Clipboard	
 powershell -command "Get-Clipboard" >> "%output_dir%\area_transferencia.txt"
-:Network Information
+::Network Information
 ipconfig /all >> "%output_dir%\ipconfig_all.txt"
 ipconfig /displaydns >> "%output_dir%\displaydns.txt"
 Netstat -a >> "%output_dir%\netstat_a.txt"
